a5ed4ada3b6ebf2d823dadb379b9788e4b97e135
Sebastiaan Bekker <SebastiaanBekker@users.noreply.github.com>
2023-05-24 21:06:10 +0200

    Week3 toegevoegd, incl cases

--- Week2shopsandlocations.py
+++ Week2shopsandlocations.py
@@ -1,36 +1,74 @@
 import random
+import math
 
-# List of existing Dutch shop names and cities for demonstration purposes
-existing_shop_names = ['Albert Heijn', 'Kruidvat', 'Jumbo', 'Hema', 'Gall & Gall']
-existing_cities = ['Amsterdam', 'Rotterdam', 'Utrecht', 'Den Haag', 'Eindhoven']
+winkelketens = ['Albert Heijn', 'Kruidvat', 'Jumbo', 'Hema', 'Gall & Gall']
+steden = ['Amsterdam', 'Rotterdam', 'Utrecht', 'Den Haag', 'Eindhoven']
 
-shops = {}
+transacties = {}
 
-# Generate 98 entries with normal size
+# deze code maakt een dictionary met 98 transacties, iedere transactie heeft een winkelnaam, locatie en bedrag en is een list
 for i in range(1, 99):
-    shop_name = random.choice(existing_shop_names)
-    location = random.choice(existing_cities)
-    amount = random.randint(100, 10000)
-    shops[shop_name] = {
-        'location': location,
-        'amount': amount
-    }
+    winkel_naam = random.choice(winkelketens) # random.choice kiest een willekeurig element uit de tuple met winkelketens
+    locatie = random.choice(steden) # random.choice kiest een willekeurig element uit de tuple met steden
+    bedrag = random.randint(100, 10000) # Dit zie je vaak met bedragen: zit zijn centen, geen euro's. Als je dit wil printen, moet je er een komma tussen zetten
+    transacties[i] = (winkel_naam, locatie, bedrag)
 
-# Add two standout entries
-shops['Faude?'] = {
-    'location': random.choice(existing_cities),
-    'amount': random.randint(10000, 50000)
-}
-shops['Ook Fraude? '] = {
-    'location': random.choice(existing_cities),
-    'amount': random.randint(10000, 50000)
-}
+# Twee rare gevollen toevoegen
+bedrag99 = random.randint(500000, 100000000)
+bedrag100 = random.randint(500000, 100000000)
+transacties[99] =  ('Albert Heijn', 'Amsterdam', bedrag99)
+transacties[100] =  ('Kruidvat', 'Eindhoven', bedrag100)
 
-# Print the dictionary
-for shop_name, shop_info in shops.items():
-    amount = shop_info['amount']
-    print(f"{shop_name}: {shop_info}")
+### Kun je zelf twee getallen kiezen en toevoegen aan de dictionary? Met bedragen die niet zo hoog zijn of juist heel hoog?
+# Je code hieronder
 
-### Kun je de fraudegevallen vinden?
-## en wat als de bedragen veranderen?
+assert len(transacties) == 102, "Je hebt geen 102 transacties in je dictionary"
+
+# De hele dictionary
+for transactie in transacties.items():
+    print(transactie)
+
+## hoe vinden we de rare gevallen?
+
+# Begin met het gemiddelde te berekenen:
+## Hier zitten twee fouten in de code, kun je die vinden?
+totaal_bedrag = 0
+if transactie in transacties.items() # juiste loop?
+    totaal_bedrag = totaal_bedrag + transactie[1][2] # dit telt de bedragen op
+
+print (totaal_bedrag)
+## Als het bedrag voor het totaal nu klopt, kun je het gemiddelde berekenen? Kijk anders even op chatpgpt ;-)
+
+gemiddelde = 0 # dit wordt dus een berekening die je nog moet maken ;-)
+
+lengte = len(transacties)
+assert  gemiddelde ==  totaal_bedrag/lengte, "Het gemiddelde is niet goed berekend" # dit is een test, als het gemiddelde niet klopt, stopt de code hier
+
+# Om de niet-standaard getallen te vinden, gebruik je vaak wiskundige formules.Hier gaan we voor alle bedragen de afstand tot het gemiddelde berekenen
+bedragen = []
+for transactie in transacties.items():
+    bedragen.append(transactie[1][2]) # dit voegt de bedragen toe aan een list
+
+# Nu de standaard deviatie, hiermee kun je kijken of er uitschieters zijn. Je berekent de afstand tot het gemiddelde
+# Dit is een functie.
+def calculate_std_dev(gemiddelde, bedragen):
+    variance = sum((bedrag - gemiddelde) ** 2 for bedrag in bedragen) / len(bedragen) # De afstand tot het gemiddelde, in het kwadraat
+    ## Dus grotere getallen worden nog groter
+
+    # Door te worteltrekken krijg je de standaard deviatie
+    std_dev = math.sqrt(variance)
+    return std_dev
+
+standaard_afwijking = calculate_std_dev(gemiddelde, bedragen)
+
+print("De rare bedragen zijn:")
+
+aantal_rare_bedragen = 0
+## een loop met weer een paar fouten ;-)
+for transactie in transacties.items():
+if transactie[1][2] > gemiddelde + standaard_afwijking * 2:
+print (transactie)
+aantal_rare_bedragen = aantal_rare_bedragen + 1
+
+assert aantal_rare_bedragen == 4, "Er zijn geen 4 rare bedragen" # dit is een test, als het gemiddelde niet klopt, stopt de code hier
 
